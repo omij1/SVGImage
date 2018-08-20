c13d651aa69c11225460731594088a3ab90b48c5
Omar Marcos Julián <omarcos@indra.es>
2018-08-20 11:27:10 +0200

    Initial Commit

---
+++ SVGImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SVGImage
+//
+//  Created by Omar Marcos Julián on 20/8/18.
+//  Copyright © 2018 Omar Marcos Julián. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
